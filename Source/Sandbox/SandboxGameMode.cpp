ad7724c6f6bd77d294954eb4ec92dde01a836cad
Yun Zhi Lin <yun@yunspace.com>
2022-01-12 22:08:54 +1100

    initial commit

---
+++ Source/Sandbox/SandboxGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SandboxGameMode.h"
+#include "SandboxCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ASandboxGameMode::ASandboxGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
